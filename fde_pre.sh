4ce6030b54d4e7a6237b27cc0a8ce574b9b5f77b
pankaz <177361438+pnkz01@users.noreply.github.com>
2026-01-17 00:08:32 +0530

    Add fde_pre.sh for enabling cryptodisk in GRUB

---
+++ fde_pre.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# enable crytpodisk in grub
+sudo echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
+sudo cat /etc/default/grub
+
+# check correct keyboard layout in /etc/default/keyboard
+sudo cat /etc/default/keyboard
+
+sudo update-initramfs -u
+# check that the cryptsetup is installed in initramfs
+lsinitramfs /boot/initrd.img-$(uname -r) | grep crypt
