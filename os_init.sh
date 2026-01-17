a56b207ab19e2a4282bc8a87069f0bdaf3bfb1f5
pankaz <177361438+pnkz01@users.noreply.github.com>
2026-01-17 16:29:20 +0530

    Remove SSH config modifications from os_init.sh
    Removed SSH configuration changes from the initialization script.

--- os_init.sh
+++ os_init.sh
@@ -12,13 +12,6 @@ ps -ef
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y build-essential htop cryptsetup cryptsetup-initramfs
 
-# change default ssh config
-sudo sed -i 's/#Port 22/Port 1111/' /etc/ssh/sshd_config
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-sudo systemctl restart sshd
-
 # disable non required services
 sudo sed -i 's/#LLMNR=yes/LLMNR=no/' /etc/systemd/resolved.conf
 sudo systemctl restart systemd-resolved
