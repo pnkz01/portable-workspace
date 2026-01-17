359dd937e9709e54a2a6414c44032ac1e687be9c
pankaz <177361438+pnkz01@users.noreply.github.com>
2026-01-17 23:40:53 +0530

    Enable cryptodisk in GRUB and verify settings
    Add configuration for full disk encryption and check settings

--- os_init.sh
+++ os_init.sh
@@ -15,3 +15,15 @@ sudo apt install -y build-essential htop cryptsetup cryptsetup-initramfs
 # disable non required services
 sudo sed -i 's/#LLMNR=yes/LLMNR=no/' /etc/systemd/resolved.conf
 sudo systemctl restart systemd-resolved
+
+# Prepare os for fde
+# enable crytpodisk in grub
+sudo sed -i '$a\GRUB_ENABLE_CRYPTODISK=y' /etc/default/grub
+sudo cat /etc/default/grub
+
+# check if correct keyboard layout is set in /etc/default/keyboard
+sudo cat /etc/default/keyboard
+
+sudo update-initramfs -u
+# check that the cryptsetup is installed in initramfs
+lsinitramfs /boot/initrd.img-$(uname -r) | grep crypt
