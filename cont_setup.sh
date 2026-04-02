527ce0ebd66970233dc85ecf7ce5ce354dcc4357
pankaz <177361438+pnkz01@users.noreply.github.com>
2026-04-02 11:44:04 +0530

    Update cont_setup.sh

--- cont_setup.sh
+++ cont_setup.sh
@@ -2,3 +2,16 @@
 set -e
 
 echo "Custom container setup commands here..."
+
+CONT_USER="dubian"
+
+# Setup services
+su $CONT_USER -c /bin/bash <<EOF
+cd /home/$CONT_USER
+
+# vs-code cli
+curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
+tar -xf vscode_cli.tar.gz
+rm vscode_cli.tar.gz
+
+EOF
